af8885646131f4d31b9fd8f21fea0da255177ce8
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 19:07:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,9 @@
 
 
 def position_taken?(board, index)
-   !(board[index] == " " || board[index] == "" || board[index] == nil || board[index] != "X" || board[index] != "O")
-
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] != "X" || board[index] != "O"
+    return true
+  end
 end
