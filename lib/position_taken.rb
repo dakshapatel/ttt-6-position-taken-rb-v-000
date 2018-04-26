7515fb8b03f23d9440670f16eb4436b17bc0f1c1
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 18:51:33 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,5 @@ board = [ "", "", " ", "", "", "", "", "", ""]
 
 def position_taken?(board, index)
    (board[index] ==  " " || board[index] == "" || board[index] == nil || board[index] != "X" || board[index] != "O")
-    
+
 end
